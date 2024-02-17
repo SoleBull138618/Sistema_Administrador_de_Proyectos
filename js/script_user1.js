c5bf9c840876aead919d2745b8ea4e4617bb5d0f
SoleBull138618 <gcsiprea@hotmail.com>
2024-02-16 23:48:05 -0700

    Script para los botones del usuario Administrador.

---
+++ js/script_user1.js
@@ -0,0 +1,41 @@
+'use strict';
+
+window.addEventListener('load', eventListeners);
+
+function eventListeners(){
+
+    document.getElementById('btnProyectos').addEventListener('click',verProyecto);
+    document.getElementById('btnEstadisticas').addEventListener('click',verProyecto);
+    document.getElementById('btnAgregarUsuarios').addEventListener('click',verProyecto);
+    document.getElementById('btnModificarUsuarios').addEventListener('click',verProyecto);
+    
+}
+
+function verProyecto(){
+    
+    let elementValue = this.getAttribute('value')
+    let newSrc = '';
+
+    switch (elementValue){
+        case 'Proyectos':
+            newSrc = 'views/proyectos.html'
+            break;
+        case 'Estadisticas':
+            newSrc = 'views/estadisticas.html'
+            break;
+        case 'Agregar Usuarios':
+            newSrc = 'views/usuarios.html'
+            break;
+        case 'Modificar Usuarios':
+            newSrc = 'views/modificar_usuarios.html'
+            break;
+        default:
+            console.log('chale')
+            break;
+    }
+
+    console.log(newSrc)
+
+    document.getElementById('framecito').src = newSrc;
+
+}
