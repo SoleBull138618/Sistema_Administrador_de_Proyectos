ffb10bb1121783a7ceca0095866d7e05ff3e1a67
SoleBull138618 <gcsiprea@hotmail.com>
2024-02-19 01:07:46 -0700

    Vista para agregar usuarios.

--- js/view_agregar_usuarios.js
+++ js/view_agregar_usuarios.js
@@ -7,14 +7,16 @@ function eventListeners(){
     document.getElementById('btnRegistrarUsuarios').addEventListener('click',registrarUsuarios);
 }
 
+console.log($('#registrar_nombre').val())
+
 function registrarUsuarios(){
 
     let form_data = new FormData();
     form_data.append('opc','guardar_usuarios');
-    form_data.append('registrarnombre');
-    form_data.append('registrarempleado');
-    form_data.append('rol');
-    form_data.append('contra');
+    form_data.append('registrarnombre',$('#registrar_nombre').val());
+    form_data.append('registrarempleado',$('#registrar_empleado').val());
+    form_data.append('rol',$('#rol').val());
+    form_data.append('contra',$('#contra').val());
 
     $.ajax({
         type: 'POST',
