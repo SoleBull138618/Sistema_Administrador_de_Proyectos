b1e5f93c73c4d212537cecb4a0661df5d45652a1
SoleBull138618 <gcsiprea@hotmail.com>
2024-02-18 18:24:57 -0700

    Script para extraer la información de la tabla usuarios.

---
+++ js/view_modificar_usuario.js
@@ -0,0 +1,16 @@
+
+let form_data = new FormData();
+form_data.append('opc','info_tabla_usuarios');
+
+$.ajax({
+    type: 'POST',
+    url: '../funciones.php',
+    // data: {opc: "info_tabla_usuarios"},
+    data: form_data,
+    dataType: 'json',
+    processData: false,
+    contentType: false,
+    success: function(respuesta) {
+        $("#tabla_usuarios").append(respuesta.info_empleado);
+    }
+})
