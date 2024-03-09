3330890b0c0b0fc0a506b1665a7dfe30ac665543
SoleBull138618 <gcsiprea@hotmail.com>
2024-03-08 23:41:16 -0700

    Script para extraer la información de la tabla usuarios.

--- js/view_modificar_usuario.js
+++ js/view_modificar_usuario.js
@@ -1,16 +1,50 @@
+'use strict';
 
-let form_data = new FormData();
-form_data.append('opc','info_tabla_usuarios');
+$(document).ready(eventListeners);
 
-$.ajax({
-    type: 'POST',
-    url: '../php/funciones.php',
-    // data: {opc: "info_tabla_usuarios"},
-    data: form_data,
-    dataType: 'json',
-    processData: false,
-    contentType: false,
-    success: function(respuesta) {
-        $("#tabla_usuarios").append(respuesta.info_empleado);
-    }
-})
+// window.addEventListener('load', eventListeners);
+
+function eventListeners(){
+
+    llenaTablaUsuarios();
+
+    $('[data-manipulation-button] input[type=button]').on('click', function(){
+
+        let parent = this.parentNode;
+        let parentRow = $(`#${parent.getAttribute('data-manipulation-button')}`)[0]; //'marco'+name;  "marco"+name; `marco${name}`
+
+        if(this.value == 'Editar'){
+            console.log('Esta editandome')
+
+            fnLlenaId(parentRow.id) //Manda a llenar #id
+
+            window.modal.showModal();
+        }
+        else if(this.value == 'Borrar'){
+
+            console.log("Esta borrandome")
+            $(parentRow).hide()
+        }
+
+    });
+}
+
+function llenaTablaUsuarios(){
+
+    let form_data = new FormData();
+    form_data.append('opc','info_tabla_usuarios');
+
+    $.ajax({
+        type: 'POST',
+        url: '../php/funciones.php',
+        // data: {opc: "info_tabla_usuarios"},
+        data: form_data,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        success: function(respuesta) {
+            $("#tabla_usuarios").append(respuesta.info_empleado);
+        }
+    })
+
+}
