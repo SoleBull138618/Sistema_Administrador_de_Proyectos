b671ba6f607ccbd78fba6caaeb70acca21a78e40
SoleBull138618 <gcsiprea@hotmail.com>
2024-03-08 23:36:21 -0700

    Login del proyecto.

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<!DOCTYPE html>
+<?php
+    include 'php/databaseconnect.php';
+    session_start();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && filter_var($_POST['username'],FILTER_VALIDATE_INT)){
+        
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        $conexion_bd = conexion_bd();
+
+        $insertarDatos = "SELECT empleado, rol FROM users WHERE empleado = '$user' AND clave = '$pass';";
+
+        if($conexion_bd){
+
+            $result = mysqli_query ($conexion_bd,$insertarDatos);
+            $row = mysqli_fetch_assoc($result);
+
+            $_SESSION['user_id'] = $row['empleado'];
+            $_SESSION['user_type'] = $row['rol'];
+
+            switch(strtoupper($row['rol'])){
+                case '1':
+                    header('Location: user1.php');
+                    exit;
+                    break;
+                case '2':
+                    header('Location: user2.php');
+                    exit;
+                    break;
+                case '3':
+                    header('Location: user3.php');
+                    exit;
+                    break;
+            }
+        }
+    }
+?>
+
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -11,11 +50,12 @@
     <body>
         <!-- Login -->
         <div class="contenedor">
-            <form class="formulario">
+            <form class="formulario" method="POST">
                 <h1 class="titulo">Bienvenido</h1>
                 <div class="campos">
                     <div class="campo__usuarios">
                         <input
+                            name="username"
                             type="number"
                             placeholder="N° Empleado"
                             required
@@ -26,20 +66,17 @@
                     <div class="campo__contraseña">
                         <input
                             type="password"
+                            name="password"
                             placeholder="Contraseña"
                             required
                             maxlength="15"
                         >
                         <img src="imagenes/help-circle-solid-24.png" alt="comentario" title="La contraseña debe tener:
 - Mínimo 8 dígitos.
-- 1 mayuscula, 1 numero y un carácter especial." class="i">
+- 1 mayuscula y 1 numero." class="i">
                     </div> 
                     <input type="submit" value="Login">
                 </div>
-
             </form>
-       
-        
-
     </body>
 </html>
