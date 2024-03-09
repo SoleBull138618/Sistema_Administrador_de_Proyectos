5320ce14a809f12a58e1f7f58bd19e681a7f6516
SoleBull138618 <gcsiprea@hotmail.com>
2024-03-08 23:53:57 -0700

    Funciones del sistema.

--- php/funciones.php
+++ php/funciones.php
@@ -15,17 +15,16 @@
             guardar_usuarios();
             break;
         case 'guardar_proyectos':
-            guardar_usuarios();
+            guardar_proyectos();
             break;
         case 'info_tabla_proyectos':
             info_tabla_proyectos();
             break;
-    }
-
-    function pruebaInfo(){
-
-        echo "perrito";
-
+        case 'comentario_old_tabla_proyectos':
+            comentario_old_tabla_proyectos();
+             break;
+        case 'actualizar_comentario_proyecto':
+            actualizar_comentario_proyecto();
     }
 
     // Evento para guardar el alta de los usuarios. 
@@ -50,14 +49,13 @@
         print json_encode($salidaJSON);
     }
 
-    // Evento para guardar el alta de los nuevos proyectos.
     function guardar_proyectos(){
 
         global $conn;
+        $proyecto= $_POST ['nombreproyecto'];
         $fecha_registro= $_POST ['fecha_alta'];
         $origen_peticion= $_POST ['origenpeticion'];
         $servicio= $_POST ['servicio'];
-        $proyecto= $_POST ['nombreproyecto'];
         $descripcion= $_POST ['descripcion'];
         $tipo_proyecto= $_POST ['tipo_proyecto'];
         $clasificacion= $_POST ['clasipro'];
@@ -66,7 +64,7 @@
         $programador= $_POST ['desarrollador'];
         $fecha_compromiso= $_POST ['fecha_compromiso'];
 
-        $insertarDatos1 = "INSERT INTO proyectos_historica VALUES ('','$fecha_registro','$origen_peticion','$servicio','$proyecto','$descripcion','$tipo_proyecto','$clasificacion','$peticion','$arquitecto','$programador','','','','$fecha_compromiso','','')";
+        $insertarDatos1 = "INSERT INTO proyectos_historica VALUES ('','$proyecto','$fecha_registro','$origen_peticion','$servicio','$descripcion','$tipo_proyecto','$clasificacion','$peticion','$arquitecto','$programador','','','','$fecha_compromiso','','')";
         $ejecutarInsertar = mysqli_query ($conn,$insertarDatos1);
     }
     
@@ -83,11 +81,12 @@
 
             while ($row = mysqli_fetch_assoc($result)){
 
-                $html_info .= "<tr>";
+                $html_info .= "<tr #empleado='#{$row['empleado']}'>";
                 $html_info .= "<td>{$row["empleado"]}</td>";
                 $html_info .= "<td>{$row["nombre"]}</td>";
                 $html_info .= "<td>{$row["rol"]}</td>";
                 $html_info .= "<td>{$row["clave"]}</td>";
+                $html_info .= "<td data-manipulation-button='#{$row['empleado']}'><input id='btnBorrarUsuarios' type='button' value='Borrar' class='btnEliminarUsuarios'> <input id='btnEditarUsuarios' type='button' value='Editar' class='btnEditarUsuarios'></td>";
                 $html_info .= "</tr>";
             }
 
@@ -113,7 +112,7 @@
 
             while ($row = mysqli_fetch_assoc($result)){
 
-                $html_info .= "<tr>";
+                $html_info .= "<tr id='project_{$row['id_proyecto']}'>";
                 $html_info .= "<td>{$row["id_proyecto"]}</td>";
                 $html_info .= "<td>{$row["proyecto"]}</td>";
                 $html_info .= "<td>{$row["fecha_registro"]}</td>";
@@ -131,6 +130,7 @@
                 $html_info .= "<td>{$row["fecha_compromiso"]}</td>";
                 $html_info .= "<td>{$row["comentarios"]}</td>";
                 $html_info .= "<td>{$row["ultima_actualizacion"]}</td>";
+                $html_info .= "<td data-manipulation-button='project_{$row['id_proyecto']}'><input type='button' value='Editar' class='btnEditarUsuarios'></td>";
                 $html_info .= "</tr>";
             }
             
@@ -141,4 +141,54 @@
         $salidaJSON = array('info_proyectos' => $html_info, 'estado' => $estado);
         print json_encode($salidaJSON);
 
-    }          
+    }      
+    
+    // Evento para consultar los comentarios de la tabla proyectos.
+    function comentario_old_tabla_proyectos(){
+
+        $id = $_POST['id'];
+
+        global $conn;
+        $query = "SELECT comentarios FROM proyectos_historica where id_proyecto='$id';";
+        $html_info = '';
+        $estado = false;
+
+        if($result = mysqli_query($conn, $query)){
+
+            while ($row = mysqli_fetch_assoc($result)){
+
+                $html_info .= "<tr>";
+                $html_info .= "<td>{$row["comentarios"]}</td>";
+                $html_info .= "</tr>";
+            }
+            
+            $estado = true;
+        }
+
+        mysqli_close($conn);
+        $salidaJSON = array('old_comentario' => $html_info, 'estado' => $estado);
+        print json_encode($salidaJSON);
+
+    }
+
+    function actualizar_comentario_proyecto(){
+
+        $update_avance_actual = $_POST ['update_avance_actual'];
+        $update_comentarios= $_POST ['update_comentarios'];
+        $old_comentarios= $_POST ['old_comentarios'];
+        $id= $_POST ['id'];
+
+        global $conn;
+        
+        // $new_comentario = $old_comentarios.'|'.$update_comentarios;
+
+        $insertarDatos = "UPDATE proyectos_historica SET avance_actual='$update_avance_actual',comentarios='$update_comentarios' WHERE id_proyecto='$id'";
+
+        $ejecutarInsertar = mysqli_query ($conn,$insertarDatos);
+
+        // $estado = true;
+
+        // mysqli_close($conn);
+        // $salidaJSON = array('estado' => $estado);
+        // print json_encode($salidaJSON);
+    }      
